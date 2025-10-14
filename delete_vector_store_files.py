8fe0792580d92f2461348452fb1101449fd5e07b
Jake <jakewendt@gmail.com>
2025-10-14 15:38:26 -0700

    Included script to keep vector stores and files clean

---
+++ delete_vector_store_files.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from dotenv import load_dotenv
+_ = load_dotenv('.dbenv')
+
+from openai import OpenAI
+client = OpenAI()
+
+
+for vs in client.vector_stores.list():
+	print(vs)
+	for f in client.vector_stores.files.list(vector_store_id=vs.id):
+		print(f)
+		print(client.vector_stores.files.delete(
+			vector_store_id=vs.id,
+			file_id=f.id
+		))
+		print(client.files.delete(f.id))
+	print(client.vector_stores.delete(vector_store_id=vs.id))
+
+
+#	Delete all files.
+#
+#	for f in client.files.list():
+#		print(f)
+#		client.files.delete(f.id)
+
